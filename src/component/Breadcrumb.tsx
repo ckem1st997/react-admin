1bfcd9e59e250d85284bb910f008cee1d7b6288f
ckem1st997 <hopxc1997@gmail.com>
2023-10-20 01:40:34 +0700

    update

--- src/component/Breadcrumb.tsx
+++ src/component/Breadcrumb.tsx
@@ -3,43 +3,75 @@ import {
     EuiBreadcrumb,
     EuiHeaderBreadcrumbs
 } from '@elastic/eui';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 export default () => {
+    const navigate = useNavigate();
+
+    //
+    const location = useLocation();
+    const currentPath = location.pathname;
+    // Tạo một hàm để chuyển đổi currentPath thành breadcrumbs
+    function generateBreadcrumbs(path: string) {
+        const pathParts = path.split('/').filter(part => part !== '');
+
+        const breadcrumbs: EuiBreadcrumb[] = [];
+
+        let currentPath = '';
+
+        pathParts.forEach((part, index) => {
+            currentPath += '/' + part;
+
+            const breadcrumb: EuiBreadcrumb = {
+                text: part,
+                className: 'customClass',
+            };
+
+            // Điều hướng đến đường dẫn tương ứng khi click vào breadcrumb
+            if (index === 0) {
+                breadcrumb.onClick = () => {
+                    navigate('/');
+                };
+                breadcrumbs.push({
+                    text: part,
+                    onClick: (e) => {
+                        navigate("/");
+                    },
+                    className: 'customClass',
+                })
+            } else {
+                breadcrumb.onClick = () => {
+                    navigate(currentPath);
+                };
+                breadcrumbs.push({
+                    text: part,
+                    onClick: (e) => {
+                        navigate(currentPath);
+                    },
+                    className: 'customClass',
+                })
+            }
+
+          //  breadcrumbs.push(breadcrumb);
+        });
+
+        return breadcrumbs;
+    }
+
+console.log(generateBreadcrumbs(currentPath))
+    //
 
     const renderBreadcrumbs = () => {
         const breadcrumbs: EuiBreadcrumb[] = [
             {
-                text: 'Management',
-                href: '#',
+                text: 'Quản lý 1',
                 onClick: (e) => {
-                    e.preventDefault();
+                    navigate("/");
                 },
-                'data-test-subj': 'breadcrumbsAnimals',
                 className: 'customClass',
             },
             {
-                text: 'Truncation test is here for a really long item',
-                href: '#',
-                onClick: (e) => {
-                    e.preventDefault();
-                },
-            },
-            {
-                text: 'Hidden',
-                href: '#',
-                onClick: (e) => {
-                    e.preventDefault();
-                },
-            },
-            {
-                text: 'Users',
-                href: '#',
-                onClick: (e) => {
-                    e.preventDefault();
-                },
-            },
-            {
-                text: 'Create',
+                text: 'Home 1',
+                className: 'customClass',
             },
         ];
         return (
