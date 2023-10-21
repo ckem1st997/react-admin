205f4912f0e4c9b568b7c81b565014e48d59d455
ckem1st997 <hopxc1997@gmail.com>
2023-10-21 10:34:07 +0700

    update

--- src/hepler/FunctionHelper.tsx
+++ src/hepler/FunctionHelper.tsx
@@ -1,9 +1,7 @@
 export const recursiveSearch = (items: any[], query: string) => {
     return items
       .map((item) => {
-        console.log(item)
         let isMatch = false; // Biến để kiểm tra xem item hiện tại có khớp không
-  
         if (item.items) {
           const matchingChildren = recursiveSearch(item.items, query);
           if (matchingChildren.length > 0) {
@@ -12,11 +10,9 @@ export const recursiveSearch = (items: any[], query: string) => {
             item = { ...item, items: matchingChildren };
           }
         }
-  
         if (item.label.includes(query) || item.name.includes(query) || item.id.includes(query)) {
           isMatch = true;
         }
-  
         return isMatch ? item : null; // Trả về item nếu nó thỏa mãn điều kiện, ngược lại trả về null
       })
       .filter((item) => item !== null); // Lọc bỏ các item là null
